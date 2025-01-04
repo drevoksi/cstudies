a600a9451059d95250d71cae6e2c4221341456a3
drevoksi <dawwwst@gmail.com>
2025-01-04 21:23:56 +0100

    4, feat: add image loading

--- 4 - Raster Print Pattern/pattern.py
+++ 4 - Raster Print Pattern/pattern.py
@@ -9,6 +9,8 @@ from random import random
 
 def sheet(w, h):
     return [[1] * h for i in range(w)]
+def rand_sheet(w, h):
+    return [[random() for y in range(w)] for x in range(h)]
 
 sub = ((-0.25, -0.25), (0.25, -0.25), (-0.25, 0.25), (0.25, 0.25))
 def draw_circle(tone, cx, cy, rsq):
@@ -38,7 +40,27 @@ def get_tone_pattern(tone, res):
             t = tone[tx][ty]
             draw_circle(pattern, cx, cy, t * res * res / 2)
     return pattern
-    
+
+from PIL import Image
+
+def get_tones(directory, targetw):
+    img = Image.open(directory)
+    r = targetw / img.width
+    img = img.resize((int(img.width * r), int(img.height * r)))
+    w = img.width
+    h = img.height
+    tone_r = sheet(w, h)
+    tone_g = sheet(w, h)
+    tone_b = sheet(w, h)
+    for x in range(w):
+        for y in range(h):
+            col = img.getpixel((x, y))
+            tone_r[x][y] = col[0] / 255
+            tone_g[x][y] = col[1] / 255
+            tone_b[x][y] = col[2] / 255
+    print(len(tone_r), len(tone_r[0]))
+    return (tone_r, tone_g, tone_b)
+
 def get_img(tone_r, tone_g, tone_b):
     w = len(tone_r)
     h = len(tone_r[0])
@@ -48,6 +70,6 @@ def get_img(tone_r, tone_g, tone_b):
             img.putpixel((x, y), tuple(int(min(max(tone[x][y], 0), 1) * 255) for tone in (tone_r, tone_g, tone_b)))
     return img
 
-tone = [[random() for y in range(100)] for x in range(100)]
-tone_p = get_tone_pattern(tone, 10)
+tone_r, tone_g, tone_b = get_tones("img.png", 50)
+tone_p = get_tone_pattern(tone_b, 10)
 get_img(tone_p, tone_p, tone_p).show()
