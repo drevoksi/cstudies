021afcd9d86cc1f3750cdc85ac203b202af63838
drevoksi <dawwwst@gmail.com>
2025-01-07 22:56:48 +0100

    7, doc: add explanation

--- 7 - Swap Variables/swap.py
+++ 7 - Swap Variables/swap.py
@@ -10,3 +10,14 @@ print(a, b) # prints '415 1378'
 
 # two variables can be swapped without declaration of a third variable
 # it may not be more efficient, but it certainly is very impressive
+
+# another swap of integers in a similar manner uses addition and subtraction:
+#   x = x + y
+#   y = x - y   (y is still y, so (x + y) - y = x)
+#   x = x - y   (because y = x, it's (x + y) - x = y)
+
+# xor version works in a similar way; though it is commutative (x ^ y = y ^ x)
+# the property it takes advantage of is that a ^ b ^ b = a
+#   x = x ^ y
+#   y = y ^ x   ((x ^ y) ^ y = x)
+#   x = x ^ y   ((x ^ y) ^ x = y ^ x ^ x = y)
