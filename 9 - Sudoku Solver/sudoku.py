67c9a6e63da6c45ef6bbc1aa4ce9166c4a481591
drevoksi <dawwwst@gmail.com>
2025-01-11 23:30:27 +0100

    9, start: sudoku solver

---
+++ 9 - Sudoku Solver/sudoku.py
@@ -0,0 +1,56 @@
+# consider the case when there are two cells with notes [3, 7], [3, 7] in a square
+# you can't tell where exactly 3 or 7 will be, but it must be in one of those two cells
+# so neither 3 nor 7 could appear in other cells in the square
+# ... this information will solve the square further and eventually resolve the locations of 3 and 7
+# ... but I don't know yet how to formalise this rule; it can also take many weird forms
+
+# another example: [3, 4], [3, 7], [4, 7]
+# 3, 4 and 7 must be in these three cells; no matter the possibilities outside
+
+# it could be easier to consider the recursive one-at-the-time changes of the state
+
+start = [0, 4, 5,  0, 2, 0,  0, 0, 0,
+         0, 0, 0,  0, 0, 1,  0, 0, 5,
+         0, 0, 0,  0, 8, 0,  3, 0, 0,
+
+         2, 1, 0,  0, 0, 0,  0, 8, 0,
+         0, 7, 0,  0, 0, 0,  0, 1, 0,
+         0, 0, 0,  0, 0, 0,  6, 9, 3,
+
+         0, 0, 1,  9, 0, 6,  0, 0, 0,
+         6, 0, 0,  0, 0, 0,  0, 0, 0,
+         9, 0, 0,  3, 0, 0,  0, 0, 8]
+
+board = [{i for i in range(1, 10)} for i in range(81)]
+for pos in range(81):
+    number = start[pos]
+    if number == 0: continue
+    board[pos] = {number}
+
+def solve_9(positions):
+    change = False
+    # not implemented
+    return change
+
+def solve_square(square):
+    sp = (square % 3) * 3 + (square // 3) * 3 * 9
+    positions = tuple(sp + i % 3 + (i // 3) * 9 for i in range(9))
+    return solve_9(positions)
+
+def solve_row(row):
+    rp = row * 9
+    positions = tuple(rp + i for i in range(9))
+    return solve_9(positions)
+
+def solve_column(column):
+    positions = tuple(column + i * 9 for i in range(9))
+    return solve_9(positions)
+
+changed = True
+while changed:
+    changed = False
+    for i in range(9):
+        changed = changed or solve_row(i) or solve_column(i) or solve_square(i)
+
+for i in range(9):
+    print(board[i * 9 : i * 9 + 9])
