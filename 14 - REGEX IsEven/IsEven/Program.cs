743209a3640b5c29d092d51ae0fbc0c709d65a58
drevoksi <dawwwst@gmail.com>
2025-01-20 22:27:40 +0100

    14, imp: regex iseven

---
+++ 14 - REGEX IsEven/IsEven/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+string number = "7182";
+
+// \A   match at the start of the string
+// \z   match at the end of the string
+// \d   match a decimal digit
+// *    match zero or more of the previous character
+// []   match one of the characters in the brackets
+Regex isEven = new (@"\A\d*[0, 2, 4, 6, 8]\z");
+
+Console.WriteLine(isEven.IsMatch(number));
